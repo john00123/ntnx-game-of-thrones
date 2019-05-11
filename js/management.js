6da1cb193198fdc75f79a719e6ab6d128249c76f
John Torres <john.torres.hurtado@gmail.com>
2019-05-11 16:09:30 -0700

    added manangement site

--- js/management.js
+++ js/management.js
@@ -2,19 +2,38 @@ let db = firebase.firestore();
 let characters = db.collection("characters").orderBy("ID", "asc")
 
 
+
 function characterList(){
 
   characters.get().then((query) => {
 
     query.forEach((doc) => {
-      console.log(doc.data())
+
       let state = doc.data().State
       let name = doc.data().Name
-      if(state ===1){
-        $('body').append(`<div vc class='character'>
+
+      $('body').append(`<div vc class='character'>
         <h1>${name.match(/[A-Z]/g).join('')}</h1>
-        <p>${name}</p></div>`)
-      }
+        <label class="switch">
+          <input class='red-button' value='${name}'type="checkbox" ${state === 1 ? 'checked': ''}>
+          <span class="slider round"></span>
+        </label>
+        <p>${name}</p></div>`);
+
+
+  });
+
+  $('.red-button:checkbox').change(function() {
+  // this will contain a reference to the checkbox
+
+
+    let name2 = $(this).val()
+
+    if (this.checked) {
+      db.collection("characters").doc(name2).update({State:1})
+    } else {
+      db.collection("characters").doc(name2).update({State:2})
+    }
     })
     //
   })
