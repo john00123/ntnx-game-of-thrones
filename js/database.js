7b4366d08c330a54da7486fd2e856ae5debbdf5b
John Torres <john.torres.hurtado@gmail.com>
2019-05-19 20:44:54 -0700

    final push

--- js/database.js
+++ js/database.js
@@ -17,21 +17,21 @@ function characterList(){
     query.forEach((doc) => {
       let state = doc.data().State
       let name = doc.data().Name
+      let pregnant = doc.data().Pregnant
+      let kills = doc.data().Kills
+      let iron = doc.data().Iron
 
 
       if(state === 1){ c.append(`<p class="alive">${name} is <span>Alive</span></p>`)}
       if(state === 2){ c.append(`<p class="dead"> ${name} is <span>Dead</span></p>`)}
       if(state === 3){ c.append(`<p class="ww">${name}  is a <span>Wight Walker</span></p>`)}
 
+
+
       masterResponses.push(state)
 
     })
     //
-
-    c.append('<br><br><br>')
-    c.append('<p> Daenerys was <span>not</span> pregnant</p>')
-    c.append('<p><span>Arya</span> killed the Knight King</p>')
-    c.append('<p><span>No one</span> sat in the Iron throne</p>')
 
   })
   c.fadeIn('fast')
@@ -46,6 +46,9 @@ function singlePage(text){
   usersSorted.get().then((query) => {
     query.forEach((doc) => {
       let responses = doc.data().Responses
+      let pregnant = doc.data().Pregnant
+      let kills = doc.data().Kills
+      let iron = doc.data().Iron
 
       if(doc.data().Name == userName){
         $('h1').after(`<h3>${doc.data().Score} points </h3>`)
@@ -74,6 +77,15 @@ function singlePage(text){
             $('p').eq(i).addClass('correct') : $('p').eq(i).addClass('incorrect')
           }
           }
+
+          if(pregnant === 0){ c.append(`<p class="correct">You chose Daenerys is not pregnant <code>&nbsp;+1 point</code></p>`)}
+          else{ c.append(`<p class="incorrect">You chose Daenerys is  pregnant</p>`)}
+
+          if(kills === 3){ c.append(`<p class="correct">You chose Arya kills the Knight King <code>&nbsp;+2 points</code></p>`)}
+          else{ c.append(`<p class="incorrect">You did not choose Arya as the Knight King's executioner</p>`)}
+
+          if(iron === 0){ c.append(`<p class="correct">You chose no one sats in the Iron throne <code>&nbsp;+4 points</code></p>`)}
+          else{ c.append(`<p class="incorrect">You chose someone is sitting in the Iron Throne</p>`)}
         }
 
     })
